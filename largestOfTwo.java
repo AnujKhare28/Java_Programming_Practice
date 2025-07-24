cdbb3787e0f17e13728f75848f751cf29ce7762e
Anuj Khare <07anujkhare@gmail.com>
2025-07-24 23:37:00 +0530

    Add files via upload

---
+++ largestOfTwo.java
@@ -0,0 +1,14 @@
+
+public class largestOfTwo {
+    public static void main(String [] args){
+        int a = 10;
+        int b = 20;
+        
+        if (a > b){
+            System.out.println("The Largest number is " + a);
+        }
+        else {
+            System.out.println("The Largest number is " + b);
+        }
+    }
+}
