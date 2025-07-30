5f9686a9b2f17da07ed8607e2b41d6a7a2a1f559
Anuj Khare <07anujkhare@gmail.com>
2025-07-30 23:41:52 +0530

    Add files via upload

---
+++ dataTypeOver.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class dataTypeOver {
+    public static int sum (int a, int b) {
+        return a + b;
+    }
+    public static float sum (float a, float b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(4,6));
+        System.out.println(sum(2.4f,3.5f));
+    }
+}
