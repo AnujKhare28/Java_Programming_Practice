5a6e1211065a6c6862b5df6efef5cd8c89ee0e32
Anuj Khare <07anujkhare@gmail.com>
2025-08-04 23:31:39 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Pyramid {
+    public static void Pyd(int n){
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= (n-i); j++){
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Pyd(5);
+    }
+}
