5d7b32ff0504e99957267e0caf254abcfb75572a
Anuj Khare <07anujkhare@gmail.com>
2025-08-11 22:48:15 +0530

    Add files via upload

---
+++ Data Structures & Algorithm/linearSearch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class linearSearch {
+
+    public static int linear(int number[], int key){
+        for (int i = 0; i < number.length; i++){
+            if(number[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int number[] = {2,4,6,7,8,10,12,14,16};
+        int key = 10;
+
+        int index = linear(number, key);
+        System.out.println(index);
+    }
+
+}
